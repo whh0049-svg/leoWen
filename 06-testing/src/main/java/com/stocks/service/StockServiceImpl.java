9685d45252d382dd76543def69ebb846e6b39050
Leo Wen <2447616800@qq.com>
2026-07-24 05:49:29 +0000

    Revert "Break addStock for CI demo"
    This reverts commit 3231bdaa2aa992a84465196bb520a80cde2a8bcf.

--- 06-testing/src/main/java/com/stocks/service/StockServiceImpl.java
+++ 06-testing/src/main/java/com/stocks/service/StockServiceImpl.java
@@ -32,12 +32,10 @@ public class StockServiceImpl implements StockService {
 
     @Override
     public Stock addStock(Stock stock) {
-//        stockRepository.findBySymbol(stock.symbol()).ifPresent(s -> {
-//            throw new IllegalArgumentException("Duplicate symbol: " + stock.symbol());
-//        });
-//        return stockRepository.save(stock);
-
-        throw new IllegalArgumentException("Always broken: " + stock.symbol());
+        stockRepository.findBySymbol(stock.symbol()).ifPresent(s -> {
+            throw new IllegalArgumentException("Duplicate symbol: " + stock.symbol());
+        });
+        return stockRepository.save(stock);
     }
 
     @Override
